b0c9cb2834c2841b637da9aee09c3867c0d911dc
Yitbarek-Wolde <yitbarek.ayni@gmail.com>
2024-04-09 21:39:33 -0500

    atlas cloud ready

--- users/users.router.ts
+++ users/users.router.ts
@@ -6,7 +6,7 @@ UserRouter.post("/signup", express.json(), post_SignUp);
 UserRouter.post("/signin", express.json(), post_SignIn);
 UserRouter.patch("/:user_id", deActivate_User);
 
-//
+
 
 //UserRouter.use("/:user_id/picture", VerifyUser, PicRouter);
 
